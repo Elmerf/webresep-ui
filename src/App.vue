6c3ea056e4f6b1c809f6f20fb3308399fc94b8e0
rahmanisaze <83899627+rahmanisaze@users.noreply.github.com>
2021-05-27 19:13:01 +0700

    update App.vue
    ketinggalan

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,6 @@
 
 <style lang="scss">
 #app {
-  height: 100%;
-  width: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
