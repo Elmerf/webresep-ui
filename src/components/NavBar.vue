d888432452f16e1b9262f0f2782ebca1a4d3e9d8
rahmanisaze <83899627+rahmanisaze@users.noreply.github.com>
2021-06-26 21:46:56 +0700

    Navbar Setting
    menambahkan setting pada navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -53,6 +53,14 @@
               <button type="button" class="dropdown-item" v-on:click="logout">
                 Logout
               </button>
+              <button
+                type="button"
+                class="dropdown-item"
+                data-bs-toggle="modal"
+                data-bs-target="#setting"
+              >
+                Setting Akun
+              </button>
             </ul>
           </li>
         </ul>
@@ -87,7 +95,6 @@
                   class="form-control"
                   id="exampleInputEmail1"
                   aria-describedby="emailHelp"
-                  required
                   placeholder="Email Address"
                   v-model="email"
                 />
@@ -101,6 +108,12 @@
                   placeholder="Password"
                   v-model="password"
                 />
+                <div
+                  v-if="password.length > 1 && password.length < 6"
+                  class="text-danger"
+                >
+                  Password should be greater than 6
+                </div>
               </div>
             </form>
           </div>
@@ -113,6 +126,67 @@
               v-on:click="Login"
             >
               Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Modal setting -->
+    <div
+      class="modal fade"
+      id="setting"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Setting Akun</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label"
+                  >Email address</label
+                >
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  v-model="email"
+                  value="email"
+                />
+                {{ this.$session.get("user") }}
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputEmail1">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="exampleInputPassword1"
+                  v-model="password"
+                />
+                {{ this.$session.get("password") }}
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-warning"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+              v-on:click="Login"
+            >
+              Update
             </button>
           </div>
         </div>
@@ -145,6 +219,7 @@ export default {
           if (res.status == 200) {
             this.$session.start();
             this.$session.set("user", this.email);
+            this.$session.set("password", this.password);
             this.$router.replace("/dashboard");
           }
         })
