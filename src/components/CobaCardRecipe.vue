23fb1c731c06dab17891e5caf166abac156eb0cf
Elmer Fiqi <elmerfiqi@gmail.com>
2021-07-07 18:26:46 +0700

    Add Resep Berhasil

---
+++ src/components/CobaCardRecipe.vue
@@ -0,0 +1,77 @@
+<template>
+  <ul class="cards">
+    <li class="cards_item">
+      <div class="card">
+        <div><img class="card_image" :src="recipe.image" /></div>
+        <div class="card_content">
+          <h2 class="card_title">{{ recipe.namaresep }}</h2>
+          <p class="card_text">
+            {{ recipe.deskripsi }}
+          </p>
+          <button class="btn card_btn" href="../components/DetailResep.vue">
+            Lihat Resep
+          </button>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "CobaCardRecipe",
+  props: ["recipe"],
+};
+</script>
+
+<style>
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.cards_item {
+  display: flex;
+  padding: 1rem;
+}
+
+.card {
+  background-color: #ffffff;
+  border-radius: 0.25rem;
+  box-shadow: 0 20px 40px -14px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+.card_content {
+  padding: 1rem;
+  background: #ffffff;
+}
+
+.card_title {
+  color: #000000;
+  font-size: 1.1rem;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-transform: capitalize;
+  margin: 0px;
+}
+
+.card_text {
+  color: #000000;
+  font-size: 0.875rem;
+  line-height: 1.5;
+  margin-bottom: 1.25rem;
+  font-weight: 400;
+}
+
+.card_image {
+  max-height: 12rem;
+  max-width: auto;
+  vertical-align: middle;
+}
+</style>
--- src/components/DashboardRecipe.vue
+++ src/components/DashboardRecipe.vue
@@ -1,25 +1,50 @@
 <template>
   <div style="width: 80%" class="mx-auto">
-    <h3 class="my-3 text-start">Resepku</h3>
-    <div v-if="Object.keys(recipes).length === 0" class="text-muted">
+    <h3 class="mt-3 text-start">Resepku</h3>
+    <button class="btn btn-warning btn-sm mb-3" v-on:click="AddResep">
+      Tambah Resep
+    </button>
+    <div
+      v-if="Object.keys(recipes).length === 0"
+      class="text-muted text-center"
+    >
       Kamu masih belum punya resep nih, yuk mulai berbagi resep kesukaanmu!
     </div>
-    <button class="btn btn-warning" v-on:click="AddResep">Tambah Resep</button>
+    <div v-else class="container">
+      <ul class="card" v-for="recipe in recipes" :key="recipe.id">
+        <CobaCardRecipe :recipe="recipe" />
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import CobaCardRecipe from "@/components/CobaCardRecipe.vue";
+import axios from "axios";
+
 export default {
   name: "DashboardRecipe",
+  components: {
+    CobaCardRecipe,
+  },
   data() {
     return {
-      recipes: {},
+      recipes: [],
     };
   },
   methods: {
     AddResep() {
       this.$router.replace("/dashboard/tambahresep");
     },
+    setRecipe(data) {
+      this.recipes = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/recipes/user/" + this.$session.get("_id"))
+      .then((res) => (this.recipes = res.data))
+      .catch((err) => console.log(err));
   },
 };
 </script>
